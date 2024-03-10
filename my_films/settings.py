b83f08e96ced38346cc6f44547fc392c27721d69
MERAB TODUA <merabtodua7@gmail.com>
2024-03-10 19:34:59 +0100

    adding password reset function by django password reset view and some stylings

--- my_films/settings.py
+++ my_films/settings.py
@@ -145,3 +145,11 @@ MEDIA_URL = '/media/'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#SMTP Parameters 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('EXCAVATIO')
+EMAIL_HOST_PASSWORD = os.environ.get('EXCAVATIOPASS')
+EMAIL_USE_TLS = True
--- my_films/urls.py
+++ my_films/urls.py
@@ -6,6 +6,7 @@ from django.contrib.auth.decorators import login_required
 from . import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('', include("blog.urls")),
     path('', include("users.urls")),
@@ -15,6 +16,11 @@ urlpatterns = [
     path('register/', user_not_authenticated(SignUpView.as_view(template_name='users/register.html')), name='register'),
     #here i am adding login required decorator to if user is not logged in and treys to access a logout page it goes to login page first
     path('logout/', login_required(auth_views.LogoutView.as_view(template_name='users/logout.html')), name='logout'),
+    #PASSWORD RESET VIEW
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name = "users/reset_password.html"), name ='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name = "users/password_reset_sent.html"), name ='password_reset_done'),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name = "users/password_reset_form.html"), name ='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name = "users/password_reset_done.html"), name ='password_reset_complete')
 
 ]
 
