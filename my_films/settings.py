e18b4c991485b5d088a6e056930eef1e4f21e4fc
MERAB TODUA <merabtodua7@gmail.com>
2024-05-28 01:59:11 +0200

    Update requirements and configuration for Heroku deployment

--- my_films/settings.py
+++ my_films/settings.py
@@ -85,13 +85,7 @@ DATABASES = {
     }
 }
 
-import dj_database_url
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-    )
-}
 
 
 # Password validation
