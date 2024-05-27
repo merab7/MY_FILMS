9cd8b27a4b00d59622905aa42fdc0026721b499e
MERAB TODUA <merabtodua7@gmail.com>
2024-05-28 01:26:40 +0200

    Initial commit

--- my_films/settings.py
+++ my_films/settings.py
@@ -85,6 +85,14 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
